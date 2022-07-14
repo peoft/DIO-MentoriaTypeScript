424b945815bda080011783beaae9fc5550601c05
peoft <paulo.ostermann.filho@gmail.com>
2022-07-14 20:26:52 -0300

    Definindo o retorno de uma função

--- app.ts
+++ app.ts
@@ -1,13 +1,7 @@
-type input = number | string;
+type input = number;
 
-function somarValores(input1:number | string, input2: number | string) {
-    if (typeof input1 === 'string' || typeof input2 === 'string') {
-        return input1.toString() + input2.toString();
-    } else {
-        return input1 + input2;
-    }    
+function somarValores(input1:number, input2: number):number {
+    return input1 + input2;
 }
 
-console.log(somarValores(1,5));
-console.log(somarValores('ola',', tudo bem'));
-console.log(somarValores('que dia e hoje? ', 10));
+console.log(somarValores(7,5));
