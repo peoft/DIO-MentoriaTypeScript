30e14e047b3c6a4b0f5540c659b9207a17b9f303
peoft <paulo.ostermann.filho@gmail.com>
2022-07-14 21:32:40 -0300

    Callback

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 "use strict";
-function somarValores(input1, input2) {
-    return input1 + input2;
+function somarValoresNumericosETratar(input1, input2, callback) {
+    let resultado = input1 + input2;
+    return callback(resultado);
 }
-console.log(somarValores(7, 5));
-printaValoresNumericos(3, 6);
 function printaValoresNumericos(numero1, numero2) {
     console.log(numero1 + numero2);
 }
+function aoQuadrado(numero) {
+    return numero * numero;
+}
+function dividirPorEleMesmo(numero) {
+    return numero / numero;
+}
+console.log(somarValoresNumericosETratar(5, 3, aoQuadrado));
+console.log(somarValoresNumericosETratar(5, 3, dividirPorEleMesmo));
--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-type input = number;
-
-function somarValores(input1:number, input2: number):number {
-    return input1 + input2;
+function somarValoresNumericosETratar(input1:number, input2: number, callback: (numero: number) => number):number {
+    let resultado = input1 + input2;
+    return callback(resultado);
 }
-
-console.log(somarValores(7,5));
-printaValoresNumericos(3, 6);
 
 function printaValoresNumericos(numero1: number, numero2: number):void {
     console.log(numero1 + numero2);
 }
+
+function aoQuadrado(numero: number):number{
+    return numero * numero;
+}
+
+function dividirPorEleMesmo(numero: number):number {
+    return numero / numero;
+}
+
+console.log(somarValoresNumericosETratar(5, 3, aoQuadrado));
+console.log(somarValoresNumericosETratar(5, 3, dividirPorEleMesmo));
